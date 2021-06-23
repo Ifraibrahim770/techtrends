9e18c287e913d9d6712c9b0d4fdb4c4a88503fe6
Ibrahim Diba <ibrahim.diba@turnkeyafrica.com>
2021-06-23 09:54:11 +0300

    url anomalies

--- buy_it_v1/urls.py
+++ buy_it_v1/urls.py
@@ -22,8 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('site/admin/', admin.site.urls),
     path('', include('store.urls')),
-    path('accounts/', include('allauth.urls')),
-    path('api-auth/', include('rest_framework.urls'))
+
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
