cc463178ccc9edaffe6fb498ff31dffc23889782
Ibrahim Diba <ibrahim.diba@turnkeyafrica.com>
2021-07-28 10:33:40 +0300

    debug false

--- buy_it_v1/settings.py
+++ buy_it_v1/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$0f4=_aor-%hugor7+@kmy)5cc_6bxx&-%+=xd*b0-k$w+-t^!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
